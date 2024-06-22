892a4286cf61db7fa01f4f1bd42d0bf5e6472ed7
Zyravel <mozyrik@mail.ru>
2024-06-22 15:04:57 +0300

    3.3 start

--- Zyravlev_3.3.py
+++ Zyravlev_3.3.py
@@ -1,19 +1,30 @@
 import requests
 import pprint
+import datetime
+
+access_token = 'vk1.a.BFNf2GsigyNZa_f4mL5rSR6U0bTNmgEm97DcUkzzaU6tCPQMEa78LAnOWyaj0gw59ErzTOy1x9C7_Eoa7S8emLQppXbfDKB3hs3o1yUA0aWxt04t8H4wiCdZV3AndO7a5RToLim_Yhj3JQxpyKqKiwLt8BpZl6Uqy1HGQVMbGwsEtuCZXGoXWxHSrfSR7eyU'
 
 
-access_токеn = 'vk1.a.a3Yl8wn08zpqLPhPjVSr2vRVaCz7NMXi5rZQyWxaaOq5kM_qsFPMWyQuZDVRBSCmshVYJmBkLEF0IaqlZJUldGVVFuxwCNkYO6vYPkkEHprLXP_5r1j4loKyawnb1GZyL4tzaC8lOgixRocQ_dGe8pBMI7bOsw8FvKfhMWxUr_UAWsPPSNnYSjHB4vVt5tPX'
+user_input = 443217
 
 
-user_input = input('VK: ')
-
-
-url = "https://api.vk.com/method/users.get"
+url = "https://api.vk.com/method/friends.get"
 
 params = {
-    "user_ids": user_input,
-    "access_token": access_токеn,
-    "v": "5.131"
+"user_id": user_input,
+"fields" : "last_seen",
+"order": 'name',
+"access_token": access_token,
+"v": "5.131"
 }
-response = requests.get(url, params=params).json()
-pprint.pprint(response)
+response = requests.get(url, params=params)
+
+
+
+user_info = response.json()
+pprint.pprint(user_info)
+
+# print(user_info['response'])
+# for i in user_info['response']['items']:
+    # print(i)
+    # print(i['first_name'], i['last_name'], datetime.datetime.fromtimestamp(i['last_seen']['time']))
