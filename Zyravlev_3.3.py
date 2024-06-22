09553e105c31fca4bec44d71034ffdf6e7a37d47
Zyravel <mozyrik@mail.ru>
2024-06-22 21:14:40 +0300

    3.3 bagfix

--- Zyravlev_3.3.py
+++ Zyravlev_3.3.py
@@ -1,10 +1,10 @@
 import requests
 import datetime
-
+import pprint
 access_token = 'vk1.a.BFNf2GsigyNZa_f4mL5rSR6U0bTNmgEm97DcUkzzaU6tCPQMEa78LAnOWyaj0gw59ErzTOy1x9C7_Eoa7S8emLQppXbfDKB3hs3o1yUA0aWxt04t8H4wiCdZV3AndO7a5RToLim_Yhj3JQxpyKqKiwLt8BpZl6Uqy1HGQVMbGwsEtuCZXGoXWxHSrfSR7eyU'
 
-user_input = input('Введиите id или screen_name пользователя: ')
-
+# user_input = input('Введиите id или screen_name пользователя: ')
+user_input = 443217
 url_friends = "https://api.vk.com/method/users.get"
 
 
@@ -29,19 +29,54 @@ params = {
 response = requests.get(url_friends, params=params)
 
 user_info = response.json()
-# print(user_info)
+pprint.pprint(user_info)
 
-# print(user_info['response'])
-cnt = 0
+
+cnt = user_info['response']['count']
+
+
+c = 0
+test = list()
 try:
     for i in user_info['response']['items']:
-        cnt += 1
+        test.append(i['last_name'])
+        c += 1
+        # if i['last_seen']['time']:
+        #     print('yes')
+except:
+    c += 1
+
+cc = 0
+test2 = list()
+try:
+    for i in user_info['response']['items']:
+        test2.append([i['last_name'] ,i['last_seen']['time']])
+        cc += 1
+        # if i['last_seen']['time']:
+        #     print('yes')
+except:
+    cc += 1
+print(len(test), len(test2))
+for i in range(len(test2)):
+    print(i+1, test[i], test2[i])
+print(test)
+
+
+x = 0
+try:
+    for i in user_info['response']['items']:
+        x += 1
+
         name = i['first_name']
         l_name = i['last_name']
-        date = datetime.datetime.fromtimestamp(i['last_seen']['time'])
-        print(f"{name} {l_name} был в сети: {date}")
-        # print(i['first_name'], i['last_name'], datetime.datetime.fromtimestamp(i['last_seen']['time']))
-except :
-    cnt += 1
-print(f'Всего {cnt} друзей')
+        date = i['last_seen']['time']
+        print(f"{i['first_name']} {i['last_name']} был в сети: {date}")
 
+
+except :
+    x += 1
+
+
+
+print(f'У пользователя {cnt} друга')
+print(f'У пользователя {x} друга')
